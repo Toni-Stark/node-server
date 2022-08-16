bf2f3edc86936cf069f1016d69e64663c749957d
lihao <15502354225@163.com>
2022-08-16 14:51:53 +0800

    node server is ready

--- server.js
+++ server.js
@@ -1,44 +1,43 @@
 const express = require('express');
 const app = express();
 const fs = require("fs");
- 
+
 const bodyParser = require('body-parser');
 const multer  = require('multer');
- 
+
 app.use('/public', express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({ dest: '/tmp/'}).array('image'));
- 
+
 app.get('/index.html', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
 })
- 
+
 app.post('/file_upload', function (req, res) {
- 
+
    console.log(req.files[0]);  // 上传的文件信息
- 
-   const des_file = __dirname + "/" + req.files[0].originalname;
+
+   const des_file = __dirname + "/public/" + req.files[0].originalname;
    fs.readFile( req.files[0].path, function (err, data) {
         fs.writeFile(des_file, data, function (err) {
          if( err ){
               console.log( err );
          }else{
                response = {
-                   message:'File uploaded successfully', 
+                   message:'File uploaded successfully',
                    filename:req.files[0].originalname
               };
           }
-          console.log( response );
           res.end( JSON.stringify( response ) );
        });
    });
 })
- 
+
 const server = app.listen(8081, function () {
- 
+
   const host = server.address().address
   const port = server.address().port
- 
+
   console.log("应用实例，访问地址为 http://%s:%s", host, port)
- 
+
 })
